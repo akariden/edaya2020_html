830e050bc678e57f3bd9c800cead9402e8b5d49b
akariden <akaritanobe@gmail.com>
2020-07-12 20:02:45 +0900

    second commit

---
+++ js/top.js
@@ -0,0 +1,17 @@
+//$(function(){
+//  var biggestHeight = "0";
+//  $(".parent *").each(function(){
+//    if ($(this).height() > biggestHeight ) {
+//      biggestHeight = $(this).height()
+//    }
+//  });
+//  $(".parent").height(biggestHeight);
+//})
+
+//$(function(){
+//  var biggestHeight = $(".mod_leftBlock_inner").height();
+//  $(".mod_leftBlock").height(biggestHeight);
+//
+//  console.log(biggestHeight);
+////  console.log(parentHeight);
+//});
